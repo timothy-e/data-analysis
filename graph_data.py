4efe87b0cb5506651b21a29be38355af1cee4e6d
Tim Elgersma <timothy.elgersma@gmail.com>
2020-07-15 21:55:07 -0400

    refactor readers

--- graph_data.py
+++ graph_data.py
@@ -1,4 +1,4 @@
-from read_data import read_weight, read_sleep_scores, get_songs
+from read_data import SpotifyReader, Fitbit
 import matplotlib.pyplot as plt
 import matplotlib
 import numpy
@@ -24,10 +24,12 @@ def split_on_threshold(values, threshold=0):
 
 if __name__ == "__main__":
     # handle some weight data
-    weight_times, weight = zip(*read_weight())
+    read_weights = Fitbit.WeightReader.get_data()
+    weight_times = [weight.timestamp for weight in read_weights]
+    weights = [weight.weight for weight in read_weights]
 
     weight_smoothing = 5
-    smoothed_weights = smooth(weight, weight_smoothing)
+    smoothed_weights = smooth(weights, weight_smoothing)
     smoothed_weight_times = [datetime.fromtimestamp(x) for x in smooth(smooth(weight_times, weight_smoothing), 2)]
 
     weight_change = [smoothed_weights[i] - smoothed_weights[i - 1] for i in range(1, len(smoothed_weights))]
@@ -37,11 +39,11 @@ if __name__ == "__main__":
     # gain, loss = split_on_threshold(smoothed_weight_change, 0)
 
     # handle some sleep data
-    sleep_data = read_sleep_scores()
-    sleep_timestamps = [score['timestamp'] for score in sleep_data]
-    sleep_scores = [int(score['overall_score']) for score in sleep_data]
-    sleep_heart_rates = [int(score['resting_heart_rate']) for score in sleep_data]
-    sleep_restlessnesses = [float(score['restlessness']) for score in sleep_data]
+    sleep_data = Fitbit.SleepScoreReader.get_data()
+    sleep_timestamps = [score.timestamp for score in sleep_data]
+    sleep_scores = [score.overall_score for score in sleep_data]
+    sleep_heart_rates = [score.resting_heart_rate for score in sleep_data]
+    sleep_restlessnesses = [score.restlessness for score in sleep_data]
 
     sleep_smoothing = 3
     smoothed_sleep_timestamps = [datetime.fromtimestamp(x) for x in smooth(sleep_timestamps, sleep_smoothing)]
@@ -63,7 +65,7 @@ if __name__ == "__main__":
     for i in range(len(axs)):
         twin_ax = axs[i].twinx()
         if (i == 0):
-            twin_ax.plot([datetime.fromtimestamp(x) for x in smooth(weight_times, 2)], smooth(weight, 2), 'b', linewidth=2, label='total weight')
+            twin_ax.plot([datetime.fromtimestamp(x) for x in smooth(weight_times, 2)], smooth(weights, 2), 'b', linewidth=2, label='total weight')
         if (i == 1):
             twin_ax.plot(smoothed_sleep_timestamps, smoothed_sleep_scores, 'b', linewidth=2, label='sleep score')
             twin_ax.set_ylabel('score')
--- read_data.py
+++ read_data.py
@@ -5,6 +5,7 @@ import os
 from pprint import pprint
 import spotipy
 from pathlib import Path
+from dataclasses import dataclass
 
 SPOTIFY_API_PUBLIC = os.environ.get('SPOTIFY_API_PUBLIC')
 SPOTIFY_API_PRIVATE = os.environ.get('SPOTIFY_API_PRIVATE')
@@ -12,122 +13,161 @@ SPOTIFY_USERNAME = os.environ.get('SPOTIFY_USERNAME')
 SPOTIFY_REDIRECT_URL = os.environ.get('SPOTIFY_REDIRECT_URL')
 SPOTIFY_SCOPE = 'user-read-recently-played'
 
-def read_spotify_data():
-    def open_file(path):
-        with path.open() as f:
-            return json.load(f)
+"""
+DATA CLASSES
+"""
+@dataclass
+class Weight:
+    timestamp: int
+    weight: float
 
-    files = Path('data/spotify').glob('StreamingHistory*.json')
-    return [
-        song
-        for file in files
-        for song in open_file(file)
-    ]
+@dataclass
+class SleepScore:
+    timestamp: int
+    overall_score: int
+    composition_score: int
+    revitalization_score: int
+    duration_score: int
+    deep_sleep_in_minutes: int
+    resting_heart_rate: int
+    restlessness: float
 
-def search_for_song(song, artist, token):
-    spotify = spotipy.Spotify(auth=token)
-    results = spotify.search(q=f'{song} {artist}', type='track')
-    first_result = results['tracks']['items'][0]
-    return first_result['id']
+    @staticmethod
+    def from_dict(d):
+        return SleepScore(
+            timestamp=datetime.datetime.strptime(
+                d['timestamp'],
+                "%Y-%m-%dT%H:%M:%SZ"
+            ).timestamp(),
+            overall_score=int(d['overall_score']),
+            composition_score=int(d['composition_score']),
+            revitalization_score=int(d['revitalization_score']),
+            duration_score=int(d['duration_score']),
+            deep_sleep_in_minutes=int(d['deep_sleep_in_minutes']),
+            resting_heart_rate=int(d['resting_heart_rate']),
+            restlessness=float(d['restlessness']),
+        )
 
+@dataclass
+class HeartRateReading:
+    timestamp: int
+    bpm: int
 
-def get_song_info(song_ids, token):
-    spotify = spotipy.Spotify(auth=token)
-    try:
-        return spotify.audio_features(song_ids)
-    except:
-        return None
-
-def get_token():
-    return spotipy.util.prompt_for_user_token(
-        username=SPOTIFY_USERNAME,
-        scope=SPOTIFY_SCOPE,
-        client_id = SPOTIFY_API_PUBLIC,
-        client_secret = SPOTIFY_API_PRIVATE,
-        redirect_uri = SPOTIFY_REDIRECT_URL
-    )
-
-def get_songs():
-    token = get_token()
-
-    streamed_songs = read_spotify_data()
-    streamed_ids = [
-        search_for_song(song['trackName'], song['artistName'], token)
-        for song in streamed_songs
-    ]
-    streamed_features = [
-        info
-        for i in range(0, len(streamed_ids), 50)
-        for info in get_song_info(streamed_ids[i:i+50], token)
-    ]
-
-    for i, feature in enumerate(streamed_features):
-        feature.update(streamed_songs[i])
-
-    return streamed_features
-
-def read_sleep_scores():
-    def rename_timestamp(datum):
-        datum['datetime'] = datum['timestamp']
-        datum['timestamp'] = datetime.datetime.strptime(
-            datum['timestamp'],
-            "%Y-%m-%dT%H:%M:%SZ"
-        ).timestamp()
-        return datum
-
-    file = Path('data/fitbit/sleep-score/sleep_score.csv')
-    with file.open() as f:
-        return [rename_timestamp(dict(x)) for x in csv.DictReader(f)]
+"""
+UTIL FUNCTIONS
+"""
 
 def extract_timestamp(date, time):
+    """
+    Returns the timestamp (epoch time) from a given date string (MM/DD/YY) and
+    time string (HH:mm:ss)
+    """
     month, day, year = [int(d) for d in date.split("/")]
     h, m, s = [int(t) for t in time.split(":")]
     return datetime.datetime(2000 + year, month, day, h, m, s).timestamp()
 
-def read_weight():
-    def open_weight_file(file):
-        with file.open() as f:
-            data = json.load(f)
-            return [(extract_timestamp(datum['date'], datum['time']), datum['weight']) for datum in data]
+"""
+READERS
+"""
+class Reader:
+    @classmethod
+    def read_data(cls, filenames):
+        def open_file(path):
+            with path.open() as f:
+                return json.load(f)
 
-    times = []
-    weight = []
-    files = sorted(Path('data/fitbit/user-site-export').glob('weight*.json'))
-    return [
-        datum
-        for file in files
-        for datum in open_weight_file(file)
-    ]
+        files = sorted(Path().glob(filenames))
+        return [
+            datum
+            for file in files
+            for datum in open_file(file)
+        ]
 
-class HeartRateReading:
-    def __init__(self, timestamp, bpm):
-        self.timestamp = timestamp
-        self.bpm = bpm
+    @classmethod
+    def get_data(cls):
+        raise NotImplementedError
 
-    def __repr__(self):
-        return f'{self.bpm} bpm at {datetime.datetime.fromtimestamp(self.timestamp)}'
+class SpotifyReader(Reader):
+    @classmethod
+    def get_data(cls):
+        streamed_songs = cls.read_data('data/spotify/StreamingHistory*.json')
+        token = cls._get_token()
 
-def read_heart_rates():
-    def open_file(path):
-        with path.open() as f:
-            return json.load(f)
+        streamed_ids = [
+            cls._search_for_song(song['trackName'], song['artistName'], token)
+            for song in streamed_songs
+        ]
+        streamed_features = [
+            info
+            for i in range(0, len(streamed_ids), 50)
+            for info in cls._get_song_info(streamed_ids[i:i+50], token)
+        ]
 
-    files = sorted(Path('data/fitbit/user-site-export').glob('heart_rate*.json'))
+        for i, feature in enumerate(streamed_features):
+            feature.update(streamed_songs[i])
 
-    return [
-        HeartRateReading(
-            extract_timestamp(*reading['dateTime'].split(' ')),
-            reading['value']['bpm']
+        return streamed_features
+
+    @classmethod
+    def _search_for_song(cls, song, artist, token):
+        spotify = spotipy.Spotify(auth=token)
+        results = spotify.search(q=f'{song} {artist}', type='track')
+        first_result = results['tracks']['items'][0]
+        return first_result['id']
+
+    @classmethod
+    def _get_song_info(cls, song_ids, token):
+        spotify = spotipy.Spotify(auth=token)
+        try:
+            return spotify.audio_features(song_ids)
+        except:
+            return None
+
+    @classmethod
+    def _get_token(cls):
+        return spotipy.util.prompt_for_user_token(
+            username=SPOTIFY_USERNAME,
+            scope=SPOTIFY_SCOPE,
+            client_id = SPOTIFY_API_PUBLIC,
+            client_secret = SPOTIFY_API_PRIVATE,
+            redirect_uri = SPOTIFY_REDIRECT_URL
         )
-        for file in files
-        for reading in open_file(file)
-    ]
 
+class Fitbit():
+    class SleepScoreReader(Reader):
+        @classmethod
+        def read_data(cls):
+            with Path('data/fitbit/sleep-score/sleep_score.csv').open() as f:
+                return [dict(x) for x in csv.DictReader(f)]
+
+        @classmethod
+        def get_data(cls):
+            data = cls.read_data()
+            return [SleepScore.from_dict(datum) for datum in data]
+
+    class WeightReader(Reader):
+        @classmethod
+        def get_data(cls):
+            data = cls.read_data('data/fitbit/user-site-export/weight*.json')
+            return [
+                Weight(extract_timestamp(datum['date'], datum['time']), datum['weight'])
+                for datum in data
+            ]
+
+    class HeartRateReader(Reader):
+        @classmethod
+        def get_data(cls):
+            data = cls.read_data('data/fitbit/user-site-export/heart_rate*.json')
+            return [
+                HeartRateReading(
+                    extract_timestamp(*datum['dateTime'].split(' ')),
+                    datum['value']['bpm']
+                )
+                for datum in data
+            ]
 
 if __name__ == '__main__':
-    # pprint(read_weight())
-    # pprint(read_sleep_scores())
-    # pprint(get_songs())
-    print(read_heart_rates()[0])
-    print(read_heart_rates()[-1])
-    pass
+    pprint(Fitbit.WeightReader.get_data()[:2])
+    pprint(Fitbit.SleepScoreReader.get_data()[:2])
+    pprint(SpotifyReader.get_data()[:2])
+    print(Fitbit.HeartRateReader.get_data()[:2])
